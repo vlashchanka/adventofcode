ddcfdcedcf94f051133a38ae504d83fabe799874
vlashchanka <vlashchanka@evolutiongaming.com>
2020-12-16 23:21:25 +0200

    Solve second part for task 1

--- src/main/scala/reportRepair/ReportRepairSolver.scala
+++ src/main/scala/reportRepair/ReportRepairSolver.scala
@@ -3,27 +3,50 @@ package reportRepair
 import scala.annotation.tailrec
 
 object ReportRepairSolver {
-  type ErrorOrNumber = Either[String, Int]
+  type ErrorOrNumbers[T] = Either[String, T]
   type Expenses = List[Int]
 
   @tailrec
-  def getPairProduct(sum: Int, list: List[Int], set: Set[Int] = Set()): ErrorOrNumber = {
+  def getNumbersEqualToSum(sum: Int, list: List[Int], set: Set[Int] = Set()): ErrorOrNumbers[(Int, Int)] = {
     if (list.isEmpty) {
       Left(s"Pair to have sum $sum was not found")
     } else {
       val diff = sum - list.head
       if (set.contains(diff)) {
-        Right(list.head * diff)
+        Right((list.head, diff))
       } else {
-        getPairProduct(sum, list.tail, Set(list.head) ++ set)
+        getNumbersEqualToSum(sum, list.tail, Set(list.head) ++ set)
       }
     }
   }
 
+  def getThreeNumbersEqualToSum(sum: Int, list: List[Int], set: Set[Int] = Set()): ErrorOrNumbers[(Int, Int, Int)] = {
+    val threeTriplets = list.map(el => (el, getNumbersEqualToSum(sum - el, list, set))).filter {
+      case (_, Right(_)) => true
+      case _ => false
+    }
+    val (num1, pair) = threeTriplets.head // any combination is valid, let's take first one
+    pair match {
+      case Right((num2, num3)) =>
+        val numbers = List(num1, num2, num3).sorted
+        Right((numbers.head, numbers(1), numbers(2)))
+      case _ => Left(s"There are no 3 numbers in the provided list which sum is equal $sum")
+    }
+  }
+
   def solve(expenses: Expenses, sum: Int): Unit = {
-    getPairProduct(sum, expenses) match {
+    getNumbersEqualToSum(sum, expenses) match {
       case Left(err) => println(err)
-      case Right(product) => println(s"Found product of 2 numbers for sum $sum is $product")
+      case Right((num1, num2)) => {
+        val product = num1 * num2
+        println(s"Found product of 2 numbers for sum $sum is $product")
+      }
+    }
+    getThreeNumbersEqualToSum(sum, expenses) match {
+      case Left(err) => println(err)
+      case Right((num1, num2, num3)) =>
+        val product = num1 * num2 * num3
+        println(s"Found product of 3 numbers ($num1 * $num2 * $num3) for sum $sum is $product")
     }
   }
 }
--- src/test/scala/ReportRepairTest.scala
+++ src/test/scala/ReportRepairTest.scala
@@ -5,8 +5,8 @@ class ReportRepairTest extends FunSuite {
   test("ReportRepairSolver - should return correct number") {
     val codes = List(1, 4, 45, 6, 10, 8)
     val sum = 16
-    val expected = Right(60)
-    val actual = ReportRepairSolver.getPairProduct(sum, codes)
+    val expected = Right(10, 6)
+    val actual = ReportRepairSolver.getNumbersEqualToSum(sum, codes)
     assert(actual === expected)
   }
 
@@ -14,7 +14,15 @@ class ReportRepairTest extends FunSuite {
     val codes = List(1, 4, 45, 6, 10, 8)
     val sum = 3
     val expected = Left("Pair to have sum 3 was not found")
-    val actual = ReportRepairSolver.getPairProduct(sum, codes)
+    val actual = ReportRepairSolver.getNumbersEqualToSum(sum, codes)
+    assert(actual === expected)
+  }
+
+  test("ReportRepairSolver - should return correct number for three numbers") {
+    val codes = List(1, 4, 45, 6, 10, 8)
+    val sum = 11
+    val expected = Right(1, 4, 6)
+    val actual = ReportRepairSolver.getThreeNumbersEqualToSum(sum, codes)
     assert(actual === expected)
   }
 }
