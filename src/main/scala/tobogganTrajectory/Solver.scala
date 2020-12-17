6116a28519a0c4afae457d49aec0f9952dd635a4
vlashchanka <vlashchanka@evolutiongaming.com>
2020-12-18 01:25:01 +0200

    Solved third task part 2

--- src/main/scala/tobogganTrajectory/Solver.scala
+++ src/main/scala/tobogganTrajectory/Solver.scala
@@ -6,22 +6,34 @@ import scala.annotation.tailrec
 
 object Solver extends Problem(2020, 3) {
   @tailrec
-  def getTreesNumber(lines: List[String], pos: Int = 0, shift :Int = 3, trees: Int = 0): Int = {
+  def getTreesNumber(lines: List[String], shiftRight :Int = 3, shiftDown: Int = 1,  pos: Int = 0, trees: Int = 0): Int = {
     if (lines.isEmpty) {
       trees
     } else {
       val line = lines.head
-      val newPos = (pos + shift) % line.length
+      val newPos = (pos + shiftRight) % line.length
       val treeOrSpace = line.charAt(newPos)
       def getTreesCountAfterMove(trees: Int): Int = {
         if (treeOrSpace == '#') trees + 1 else trees
       }
-      getTreesNumber(lines.tail, newPos, shift, getTreesCountAfterMove(trees))
+      getTreesNumber(lines.slice(shiftDown, lines.length), shiftRight, shiftDown, newPos, getTreesCountAfterMove(trees))
     }
   }
 
   def run(input: List[String]): Unit = {
     val treesCount = getTreesNumber(input.tail)
     println(s"Number of trees: $treesCount")
+
+    val treesSlopes = List(
+      getTreesNumber(input.tail, 1, 1),
+      getTreesNumber(input.tail, 3, 1),
+      getTreesNumber(input.tail, 5, 1),
+      getTreesNumber(input.tail, 7, 1),
+      getTreesNumber(input.slice(2, input.length), 1, 2),
+    )
+
+    val treesSlopesProduct = treesSlopes.map(BigInt(_)).product
+
+    println(s"Trees slopes product: $treesSlopesProduct")
   }
 }
--- src/test/scala/TobogganTrajectory.scala
+++ src/test/scala/TobogganTrajectory.scala
@@ -5,8 +5,24 @@ import tobogganTrajectory.Solver
 import scala.io.Source
 
 class TobogganTrajectory extends FunSuite {
-  test("should count trees correctly") {
+  test("should count trees correctly for shift (3,1)") {
     val list = Source.fromResource("03.example.txt").getLines().toList
     Solver.getTreesNumber(list.tail) shouldBe 7
+  }
+  test("should count trees correctly for shift (1,1)") {
+    val list = Source.fromResource("03.example.txt").getLines().toList
+    Solver.getTreesNumber(list.tail, 1, 1) shouldBe 2
+  }
+  test("should count trees correctly for shift (5,1)") {
+    val list = Source.fromResource("03.example.txt").getLines().toList
+    Solver.getTreesNumber(list.tail, 5, 1) shouldBe 3
+  }
+  test("should count trees correctly for shift (7,1)") {
+    val list = Source.fromResource("03.example.txt").getLines().toList
+    Solver.getTreesNumber(list.tail, 7, 1) shouldBe 4
+  }
+  test("should count trees correctly for shift (1,2)") {
+    val list = Source.fromResource("03.example.txt").getLines().toList
+    Solver.getTreesNumber(list.slice(2, list.length), 1, 2) shouldBe 2
   }
 }
