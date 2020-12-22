7d4313fc32549235b27773c2699e9c594413b751
vlashchanka <vlashchanka@evolutiongaming.com>
2020-12-22 23:51:28 +0200

    Refactor from recursion

--- src/main/scala/passportPolicy/Solver.scala
+++ src/main/scala/passportPolicy/Solver.scala
@@ -2,49 +2,29 @@ package passportPolicy
 
 import aocd.Problem
 
-import scala.annotation.tailrec
-
 object Solver extends Problem(2020, 4) {
   type Passport = Map[String, String]
 
-  def newPassport(): Passport = {
-    Map()
+  def isValidPassport(passport: Passport): Boolean = {
+    val CountryId = "cid"
+    val RequiredFieldsCount = 7
+    val counted = passport.count({
+      case (countryId, _) => countryId != CountryId
+    })
+    counted == RequiredFieldsCount
   }
 
   def getValidPassportsCount(lines: List[String]): Int = {
-    val fieldsExtractor = """(\w+):([#\w]+)""".r
-
-    val CountryId = "cid"
-    val RequiredFieldsCount = 7
-
-    def isValidPassport(passport: Passport): Boolean = {
-      val counted = passport.count({
-        case (countryId, _) => countryId != CountryId
-        case _ => false
-      })
-      counted == RequiredFieldsCount
-    }
-
-    @tailrec
-    def combinePassports(lines: List[String], passports: Int = 0, currentPassport: Passport): Int = {
-      def countPassports: Int = {
-        passports + (if (isValidPassport(currentPassport)) 1 else 0)
-      }
-      lines match {
-        case Nil => countPassports
-        case head :: tail =>
-          head match {
-            case "" => combinePassports(tail, countPassports, newPassport())
-            case _ =>
-              combinePassports(tail, passports, (fieldsExtractor findAllIn head)
-                .map({ case fieldsExtractor(key, value) => key -> value })
-                .toMap ++ currentPassport
-              )
-          }
-      }
-    }
-
-    combinePassports(lines, 0, newPassport())
+    val PassportField = """(\w+):([#\w]+)""".r
+    lines
+      .mkString(" ")
+      .split("  ")
+      .map(passportFields =>
+        (PassportField findAllIn passportFields)
+          .map({ case PassportField(name, value) => name -> value })
+          .toMap
+      )
+      .count(isValidPassport)
   }
 
   def run(input: List[String]): Unit = {
